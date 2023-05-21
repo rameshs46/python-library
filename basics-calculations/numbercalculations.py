0c35ae4a720194e7c2416eb812941570ae94e25b
Ramesh Sengamalai <rsengamalai@gmail.com>
2023-05-21 16:48:30 +0800

    Initated code for general python library

---
+++ basics-calculations/numbercalculations.py
@@ -0,0 +1,16 @@
+# Program No: 1
+# Description: Area of Rectangle
+def areareactangle(self):
+    l = int(input("l=?"))
+    b = int(input("b=?"))
+    area = l * b
+    return area
+
+
+# Program No.2
+# Description: Product of two real numbers
+def productrealnumbers(self):
+    a = float(input("a=?"))
+    b = float(input("b=?"))
+    c = a * b
+    return c
